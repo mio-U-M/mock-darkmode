cf78c5108a676e64c87407f6e1dd5191df0f4c8e
mio tagami <cre30r0ad@gmail.com>
2020-12-07 07:36:19 +0900

    一番シンプルな機構はできた

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,40 @@ const view = document.querySelector(".js-view");
 const manager = new ShaderManager(view);
 
 manager.init();
+
+// check dark mode
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+function checkColorMode(darkModeOn) {
+    if (darkModeOn) {
+        // Dark
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    } else {
+        // Light
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
+}
+
+darkModeMediaQuery.addListener(e => {
+    const darkModeOn = e.matches;
+    checkColorMode(darkModeOn);
+});
+
+const darkModeOn = darkModeMediaQuery.matches;
+checkColorMode(darkModeOn);
+
+const btn = document.querySelector("#btn-mode");
+
+// クリックした時の挙動
+btn.addEventListener("click", () => {
+    if (document.body.classList.contains("light-theme")) {
+        // ダークモード
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    } else {
+        // ライトモード
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
+});
